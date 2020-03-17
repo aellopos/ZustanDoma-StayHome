e929700c632c600e5eb5e2e1b8fd2e3e03af3099
Michal Kučera <m-kucera@outlook.com>
2020-03-17 15:58:40 +0100

    Map improvement, player move
    added:
    - player
    - map (special thanks to @filiptronicek)
    - player movement
    - code improvement

--- script.js
+++ script.js
@@ -1,58 +1,103 @@
 let canvas = document.querySelector("#canvas"),
+    ctx = canvas.getContext("2d"),
+    blockSize = 30,
     width = 600,
+    keys = [],
     height = 600;
 
 let board = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+    [1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1],
+    [1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1],
+    [1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1],
+    [1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1],
+    [1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1],
+    [1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ];
 
 let player = {
-    x: 0,
-    y: 0
+    x: 8,
+    y: 1
 };
+
+let hero = new Image();
+hero.src = "filip.png";
 
 canvas.width = width;
 canvas.height = height;
 
-function draw() {
-    let blockSize = 30;
-    let ctx = canvas.getContext('2d');
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-    ctx.clearRect(0, 0, width, width);
+function generateBoard() {
     ctx.fillStyle = "white";
-    
+
     for (var y = 0; y < board.length; y++) {
         for (var x = 0; x < board[y].length; x++) {
-         
             if (board[y][x] === 1) {
                 ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
+                //ctx.drawImage(hero, x * blockSize, y * blockSize, blockSize, blockSize);
             }
         }
     }
-
-    ctx.beginPath();
-    ctx.fillStyle = "blue";
-    ctx.fillRect(10 * blockSize, 1 * blockSize, blockSize, blockSize);
-    ctx.fill();
 }
+
+function movement() {
+    if ((keys[39] || keys[68]) && canMove(player.x + 1, player.y)) {
+        // šipka doprava
+        player.x++;
+    }
+
+    if ((keys[37] || keys[65]) && canMove(player.x - 1, player.y)) {
+        // šipka doleva
+        player.x--;
+    }
+
+    if ((keys[38] || keys[87]) && canMove(player.x, player.y - 1)) {
+        // šipka nahoru
+        player.y--;
+    }
+
+    if ((keys[40]) && canMove(player.x, player.y + 1)) {
+        // šipka dolů
+        player.y++;
+    }
+}
+
+function canMove(x, y) {
+    return (y >= 0) && (y < board.length) && (x >= 0) && (x < board[y].length) && (board[y][x] != 1);
+}
+
+
+function draw() {
+    ctx.clearRect(player.x * blockSize, player.y * blockSize, blockSize, blockSize);
+
+    generateBoard();
+    movement();
+
+    ctx.drawImage(hero, player.x * blockSize, player.y * blockSize, blockSize, blockSize);
+}
+
+document.body.addEventListener("keydown", function (e) {
+    keys[e.keyCode] = true;
+
+    draw();
+});
+
+document.body.addEventListener("keyup", function (e) {
+    keys[e.keyCode] = false;
+
+    draw();
+});
 
 window.addEventListener("load", draw);
