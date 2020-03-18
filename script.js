cc685f57a205c2e92d0710dc06e29a789281ba9b
Michal Kučera <m-kucera@outlook.com>
2020-03-18 10:07:25 +0100

    Graphics added

--- script.js
+++ script.js
@@ -33,8 +33,15 @@ let player = {
     y: 1
 };
 
+let game = {
+    timeElement: document.getElementById('time')
+};
+
 let hero = new Image();
-hero.src = "filip.png";
+hero.src = "images/down.png";
+
+let wall = new Image();
+wall.src = "images/wall.png";
 
 canvas.width = width;
 canvas.height = height;
@@ -45,8 +52,7 @@ function generateBoard() {
     for (var y = 0; y < board.length; y++) {
         for (var x = 0; x < board[y].length; x++) {
             if (board[y][x] === 1) {
-                ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
-                //ctx.drawImage(hero, x * blockSize, y * blockSize, blockSize, blockSize);
+                ctx.drawImage(wall, x * blockSize, y * blockSize, blockSize, blockSize);
             }
         }
     }
@@ -55,21 +61,25 @@ function generateBoard() {
 function movement() {
     if ((keys[39] || keys[68]) && canMove(player.x + 1, player.y)) {
         // šipka doprava
+        hero.src = "images/right.png";
         player.x++;
     }
 
     if ((keys[37] || keys[65]) && canMove(player.x - 1, player.y)) {
         // šipka doleva
+        hero.src = "images/left.png";
         player.x--;
     }
 
     if ((keys[38] || keys[87]) && canMove(player.x, player.y - 1)) {
         // šipka nahoru
+        hero.src = "images/up.png";
         player.y--;
     }
 
     if ((keys[40]) && canMove(player.x, player.y + 1)) {
         // šipka dolů
+        hero.src = "images/down.png";
         player.y++;
     }
 }
@@ -88,13 +98,13 @@ function draw() {
     ctx.drawImage(hero, player.x * blockSize, player.y * blockSize, blockSize, blockSize);
 }
 
-document.body.addEventListener("keydown", function (e) {
+document.body.addEventListener("keydown", function(e) {
     keys[e.keyCode] = true;
 
     draw();
 });
 
-document.body.addEventListener("keyup", function (e) {
+document.body.addEventListener("keyup", function(e) {
     keys[e.keyCode] = false;
 
     draw();
