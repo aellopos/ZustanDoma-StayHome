072856fa8624dbe855846a0a50167b546088c098
Michal Kučera <m-kucera@outlook.com>
2020-03-23 14:38:47 +0100

    Timer update

--- script.js
+++ script.js
@@ -146,7 +146,6 @@ function endGame(type) {
 
 // provádí pravidelný odpočet času
 function timer(time) {
-<<<<<<< HEAD
     function startTimer(duration, display) {
         var timer = duration,
             minutes,
@@ -167,28 +166,6 @@ function timer(time) {
     }
     display = document.querySelector("#time");
     startTimer(time, display);
-=======
-  function startTimer(duration, display) {
-    var timer = duration,
-      minutes,
-      seconds;
-    setInterval(function() {
-      minutes = parseInt(timer / 60, 10);
-      seconds = parseInt(timer % 60, 10);
-
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
-
-      display.innerText = minutes + ":" + seconds;
-
-      if (--timer < 0) {
-        timer = 0;
-      }
-    }, 1000);
-  }
-  display = document.querySelector("#time");
-  startTimer(time, display);
->>>>>>> master
 }
 
 function movement() {
