9f0b681d4c5dc0f6dd00407110502f91b306eff1
Michal Kučera <m-kucera@outlook.com>
2020-03-25 20:16:30 +0100

    VI. day update

--- Tutorial-pieces/VI/script.js
+++ Tutorial-pieces/VI/script.js
@@ -2,6 +2,7 @@ let width = 600;
 let height = 600;
 let blockSize = 30;
 let ctx = canvas.getContext("2d");
+let keys = [];
 //mapa našeho bludiště, 1 = zeď, 0 = cesta
 let board = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -56,10 +57,51 @@ function generateBoard() {
     }
 }
 
+function movement() {
+    if (keys[39]) {
+        // šipka doprava
+        hero.src = "images/right.png";
+        player.x++;
+    }
+
+    if (keys[37]) {
+        // šipka doleva
+        hero.src = "images/left.png";
+        player.x--;
+    }
+
+    if (keys[38]) {
+        // šipka nahoru
+        hero.src = "images/up.png";
+        player.y--;
+    }
+
+    if (keys[40]) {
+        // šipka dolů
+        hero.src = "images/down.png";
+        player.y++;
+    }
+}
+
 function draw() {
+    ctx.clearRect(player.x * blockSize, player.y * blockSize, blockSize, blockSize);
     generateBoard();
+    movement();
     ctx.drawImage(hero,player.x * blockSize, player.y * blockSize, blockSize, blockSize);
 }
 
 //poslouchač událostí, čekáme (posloucháme) na načtení stránky, pak zavoláme funkci generateBoard
 window.addEventListener("load", draw);
+
+// poslouchače událostí pro stisk klávesy
+document.body.addEventListener("keydown", function(e) {
+    keys[e.keyCode] = true;
+
+    draw();
+});
+
+document.body.addEventListener("keyup", function(e) {
+    keys[e.keyCode] = false;
+
+    draw();
+});
