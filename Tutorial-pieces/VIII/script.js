08ec6af9d6d16accae0f86ecbc59934e9d679a23
Michal Kučera <m-kucera@outlook.com>
2020-03-30 14:50:06 +0200

    VIII. day

---
+++ Tutorial-pieces/VIII/script.js
@@ -0,0 +1,112 @@
+let width = 600;
+let height = 600;
+let blockSize = 30;
+let canvas = document.querySelector("#canvas");
+let ctx = canvas.getContext("2d");
+let keys = [];
+//mapa našeho bludiště, 1 = zeď, 0 = cesta
+let board = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+    [1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1],
+    [1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1],
+    [1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1],
+    [1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1],
+    [1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1],
+    [1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
+
+// Vytváříme herní objekt pro hráče
+let player = {
+    // Pozice hráče na ose x a y
+    x: 8,
+    y: 1
+};
+
+//vytváříme novou proměnnou, do které uložíme obrázek zdi
+let wall = new Image();
+wall.src = "images/wall.png";
+
+//vytváříme novou proměnnou, do které uložíme obrázek hlavní postavy
+let hero = new Image();
+hero.src = "images/down.png";
+
+//upravujeme výšku a šířku canvasu (naší herní plochy)
+canvas.width = width;
+canvas.height = height;
+
+//funkce, která vygeneruje bludiště
+function generateBoard() {
+    for (let y = 0; y < board.length; y++) {
+        for (let x = 0; x < board[y].length; x++) {
+            if (board[y][x] === 1) {
+                ctx.drawImage(wall, x * blockSize, y * blockSize, blockSize, blockSize);
+            }
+        }
+    }
+}
+
+function movement() {
+    if (keys[39] && canMove(player.x + 1, player.y)) {
+        // šipka doprava
+        hero.src = "images/right.png";
+        player.x++;
+    }
+
+    if (keys[37] && canMove(player.x - 1, player.y)) {
+        // šipka doleva
+        hero.src = "images/left.png";
+        player.x--;
+    }
+
+    if (keys[38] && canMove(player.x, player.y - 1)) {
+        // šipka nahoru
+        hero.src = "images/up.png";
+        player.y--;
+    }
+
+    if (keys[40] && canMove(player.x, player.y + 1)) {
+        // šipka dolů
+        hero.src = "images/down.png";
+        player.y++;
+    }
+}
+
+function canMove(x, y) {
+    return (y >= 0 && y < board.length && x >= 0 && x < board[y].length && board[y][x] != 1);
+}
+
+function draw() {
+    ctx.clearRect(player.x * blockSize, player.y * blockSize, blockSize, blockSize);
+    generateBoard();
+    movement();
+    ctx.drawImage(hero,player.x * blockSize, player.y * blockSize, blockSize, blockSize);
+}
+
+//poslouchač událostí, čekáme (posloucháme) na načtení stránky, pak zavoláme funkci generateBoard
+window.addEventListener("load", draw);
+
+// poslouchače událostí pro stisk klávesy
+document.body.addEventListener("keydown", function(e) {
+    keys[e.keyCode] = true;
+
+    draw();
+});
+
+document.body.addEventListener("keyup", function(e) {
+    keys[e.keyCode] = false;
+
+    draw();
+});
